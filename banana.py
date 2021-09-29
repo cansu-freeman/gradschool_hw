1e5e755b0a5187854d85ea608827b88b1d9601d2
cansu-freeman <cansufreeman@gmail.com>
2021-09-29 09:10:23 -0400

    Changed spacing

--- banana.py
+++ banana.py
@@ -5,46 +5,38 @@ import numpy as np
 def banana(xy):
     x = xy[0]
     y = xy[1]
-    
     return 100*(y - x**2)**2 + (1 - x)**2
 
 def grad_b(xy):
     x = xy[0]
     y = xy[1]
-    
     dx = 400*x**3 - 400*x*y + 2*x - 2
     dy = 200*(y-x**2)
-    
     return np.array([dx, dy])
 
 def banana_opt_fixstep(x0, s=0.01, eps=0.0001, imax=10000):
     i = 0
     x = x0
-    
     while norm(grad_b(x)) > eps and i < imax:
         i = i + 1
         gf = grad_b(x)
         d = -gf/norm(gf)
-        x = x + s*d
-        
+        x = x + s*d       
     return x, i
 
 def banana_opt_backtrack(x0, eps=0.0001, imax=10000):
     i = 0
     x = x0
-    
     while norm(grad_b(x)) > eps and i < imax:
         i = i + 1
         gf = grad_b(x)
         d = -gf/norm(gf)
-        
         # Step size with backtracking
         s = 1
+
         while banana(x+s*d) > banana(x):
             s = s/2
-        
         x = x + s*d
-        
     return x, i
 
 def J(x):
